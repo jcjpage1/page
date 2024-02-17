e4e976322f4a1bcebb48dc37490eed74664c4cc6
jcjpage1 <160296349+jcjpage1@users.noreply.github.com>
2024-02-17 11:53:47 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var content = document.querySelector('.content');
+    window.addEventListener('scroll', function() {
+        if (isInViewport(content)) {
+            content.classList.add('show');
+        }
+    });
+
+    function isInViewport(element) {
+        var rect = element.getBoundingClientRect();
+        var windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+        return (
+            (rect.top <= windowHeight)
+        );
+    }
+});
